1f80a714da924367a708bbcb02445f053c309def
song <islbvv@gmail.com>
2025-10-04 00:33:27 +0900

    10월 4일 commint / 글작성, 회원가입 기능 추가

--- node/server.js
+++ node/server.js
@@ -18,83 +18,156 @@ const dbConfig = {
 	poolAlias: "project_01_Pool" // 풀 이름 지정
 }
 
-app.get("/posts", async (req, res) => {
-	let oracleDB
+// 서버 구동 관련
+const port = 7000;
+async function startServer() {
+	// oracle 연결을 위한 pool을 만듬
+	await makePool();
+	app.listen(port, () => {
+		console.log(
+			`[서버 가동 http://localhost:${port} (startServer)]`);
+		console.log(`------------------------------------------------`);
+	})
+};
+
+async function makePool() { // oracle 연결을 위한 pool을 만듬
 	try {
-		oracleDB = await oracledb.getConnection(dbConfig.poolAlias);
+		await oracledb.createPool(dbConfig);
+		console.log("[Pool 생성 완료 (initialize)]");
+		console.log(`------------------------------------------------`);
+	} catch (err) {
+		console.log("[Pool 생성 에러 발생 (initialize)]");
+		console.log(`------------------------------------------------`);
+		process.exit(1);
+	}
+};
+startServer(); // 서버 가동
+
+// 글 목록을 html에 전송
+app.get("/posts", async (req, res) => {
+	let connection;
+	try {
+		connection = await oracledb.getConnection(dbConfig.poolAlias);
 		let sql = `SELECT * FROM post ORDER BY post_no DESC`
-		let result = await oracleDB.execute(sql)
-		res.status(200).json(result.rows)
+		let result = await connection.execute(sql);
+		res.status(200).json(result.rows);
 	} catch (err) {
 		console.log(err);
-		console.log(`"get /posts[try]" 에러 발생`);
+		console.log(`[get /posts[try] 에러 발생]`);
+		console.log(`------------------------------------------------`);
 	} finally {
-		if (oracleDB) {
+		if (connection) {
 			try {
-				await oracleDB.close();
+				await connection.close();
+				console.log(`[get /posts] 목록 출력]`);
+				console.log(`------------------------------------------------`);
 			} catch (err) { // 에러 발생
 				console.log(err);
-				console.log(`"get /posts[finally]" 에러 발생`);
+				console.log(`[get /posts[finally] 에러 발생]`);
+				console.log(`------------------------------------------------`);
 			}
 		}
 	}
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 이 아래는 서버 구동 관련
-async function initialize() {
+// 글쓰기 데이터를 oracle로 전송
+app.post("/write", async (req, res) => {
+	let connection;
 	try {
-		await oracledb.createPool(dbConfig);
-		console.log("[project_01_Pool: 연결 성공 (initialize)]");
-	} catch (err) {
-		console.log("[project_01_Pool: 연결 성공 (initialize)]");
-		process.exit(1);
-	}
-};
+		connection = await oracledb.getConnection(dbConfig.poolAlias);
+		let sql = `
+		BEGIN
+			write_post(:write_id, :write_title, :write_content);
+		END;
+		`;
+		const binds = {
+			write_id: 'seung',
+			write_title: req.body.write_title,
+			write_content: req.body.write_content
+		};
 
-// pool이 만들어지면 서버 작동
-const port = 7000;
-async function startServer() {
-	await initialize(); // oracle 연결을 위한 pool을 만듬
-	app.listen(port, () => {
-		console.log(
-			`[project_01_Pool: 서버 on http://localhost:${port} (startServer)]`);
-	})
-};
-startServer();
+		// const binds = {};
+		// for (let item in req.body) {
+		// 	binds[item] = req.body[item];
+		// };
+
+		await connection.execute(sql, binds, { autoCommit: true });
+	} catch (err) {
+		console.log(err);
+		console.log(`[post /write[try] 에러 발생]`);
+		console.log(`------------------------------------------------`);
+	} finally {
+		if (connection) {
+			try {
+				await connection.close();
+				console.log(`[post /write 전송 완료]`);
+				console.log(`------------------------------------------------`);
+			} catch (err) { // 에러 발생
+				console.log(err);
+				console.log(`[post /write[finally] 에러 발생]`);
+				console.log(`------------------------------------------------`);
+			}
+		}
+	}
+});
+
+// 회원가입 시 중복된 id가 있는지 확인
+app.get("/", async (req, res) => {
+	let connection;
+	try {
+		connection = await oracledb.getConnection(dbConfig.poolAlias);
+		let sql = `SELECT user_id FROM users`;
+		let result = await connection.execute(sql);
+		res.status(200).json(result.rows);
+	} catch (err) {
+		console.log(err);
+		console.log(`[get / [try] 에러 발생]`);
+		console.log(`------------------------------------------------`);
+	} finally {
+		if (connection) {
+			try {
+				await connection.close();
+				console.log(`[get / 전송 완료]`);
+				console.log(`------------------------------------------------`);
+			} catch (err) { // 에러 발생
+				console.log(err);
+				console.log(`[get / [finally] 에러 발생]`);
+				console.log(`------------------------------------------------`);
+			}
+		}
+	}
+});
+
+// 회원가입 데이터를 oracle로 전송
+app.post("/sign", async (req, res) => {
+	let connection;
+	try {
+		connection = await oracledb.getConnection(dbConfig.poolAlias);;
+		let sql = `
+		BEGIN
+			add_user(:sign_id, :sign_pw, :sign_name, :sign_tel);
+		END;
+		`;
+		const binds = {};
+		for (let item in req.body) {
+			binds[item] = req.body[item];
+		};
+		await connection.execute(sql, binds, { autoCommit: true });
+	} catch (err) {
+		console.log(err);
+		console.log(`[post /sign[try] 에러 발생]`);
+		console.log(`------------------------------------------------`);
+	} finally {
+		if (connection) {
+			try {
+				await connection.close();
+				console.log(`[post /sign 전송 완료]`);
+				console.log(`------------------------------------------------`);
+			} catch (err) { // 에러 발생
+				console.log(err);
+				console.log(`[post /sign[finally] 에러 발생]`);
+				console.log(`------------------------------------------------`);
+			}
+		}
+	}
+});
--- public/app.js
+++ public/app.js
@@ -1,6 +1,8 @@
 //
+// 중복된 id가 있는지 확인하기 위한 변수
+let id_check;
 
-
+// nav 버튼 목록
 const btmIds = [
 	"home_btn",
 	"posts_btn",
@@ -9,15 +11,36 @@ const btmIds = [
 	"sign_btn",
 	"find_btn"
 ];
-const closebtn = document.querySelectorAll('.close_btn');
+
+function postBtn(btn) {
+	if (btn == 'posts_btn' || btn == 'write_add') {
+		fetch(`http://localhost:7000/posts`)
+			.then(response =>
+				response.json()
+			).then(result => {
+				document.querySelector("#posts_table tbody").innerHTML = "";
+				result.forEach(item => {
+					// 게시글 목록 출력
+					let tr = makeRow(item);
+					document.querySelector("#posts_table tbody").appendChild(tr);
+				})
+			}).catch(err =>
+				console.log(err)
+			);
+	}
+}
+
+// 닫기 버튼
+const closebtn = document.querySelectorAll(`[name="close_btn"]`);
 
 // 닫기 버튼 이벤트 생성
+function hide(e) {
+	e.preventDefault();
+	document.querySelector(`[name="home_btn"]`).classList.remove('hide');
+	this.closest('.modal').classList.add('hide');
+}
 closebtn.forEach(item => {
-	item.addEventListener('click', function(e) {
-		e.preventDefault();
-		document.querySelector(`[name="home_btn"]`).classList.remove('hide');
-		this.closest('.modal').classList.add('hide');
-	})
+	item.addEventListener('click', hide);
 })
 
 // nav 버튼 이벤트 생성
@@ -29,8 +52,7 @@ btmIds.forEach(item => {
 	};
 });
 
-// nav 버튼 이벤트 생성
-function btnClick(e) {
+function btnClick(e) { // nav 버튼 이벤트 생성
 	e.preventDefault();
 	const btnName = this.dataset.name;
 
@@ -44,27 +66,100 @@ function btnClick(e) {
 		}
 	})
 
-	// posts 버튼 (게시글 목록 출력)
-	if (btnName == 'posts_btn') {
-		fetch(`http://localhost:7000/posts`)
-			.then(response => response.json())
-			.then(result => {
-				document.querySelector("#posts_table tbody").innerHTML = "";
-				result.forEach(item => {
-					let tr = makeRow(item);
-					document.querySelector("#posts_table tbody").appendChild(tr);
-				})
-			}).catch(err => console.log(err));
+	// posts 버튼
+	postBtn(this.id);
+
+	// 중복된 id가 있는지 확인하기 위한 데이터 가져오기
+	if (btnName == 'sign_btn') {
+		fetch(`http://localhost:7000/`)
+			.then(response =>
+				response.json()
+			).then(result => {
+				id_check = result;
+			}).catch(err =>
+				console.log(err)
+			);
 	}
 }
 
-function makeRow(item) {
+function makeRow(item) { // 게시글 목록 출력
 	let fileds = ["POST_NO", "POST_TITLE", "USER_ID"];
 	let tr = document.createElement('tr');
 	fileds.forEach(filed => {
 		let td = document.createElement('td');
 		td.innerHTML = item[filed];
-		tr.appendChild(td)
+		tr.appendChild(td);
 	})
 	return tr;
 }
+
+// write 글작성
+document.getElementById('write_form').addEventListener('submit',
+	function(e) {
+		e.preventDefault();
+		let datas = {};
+		document.querySelectorAll('#write_form div').forEach(item => {
+			let result = item.querySelector('input, textarea');
+			datas[result.name] = result.value;
+		});
+		postJSON(datas, "write"); // 서버로 보내는 postJSON(data, route) 함수
+		document.querySelector(`[name="posts_btn"]`).classList.remove('hide');
+		this.closest(`[name="write_btn"]`).classList.add('hide');
+		postBtn(this.id);
+	})
+
+// 서버로 보내는 postJSON(data, route) 함수
+async function postJSON(data, route) {
+	fetch(`http://localhost:7000/${route}`, {
+		method: "post",
+		headers: { "Content-Type": "application/json;charset=UTF-8" },
+		body: JSON.stringify(data)
+	}).then(response =>
+		response.json()
+	).catch(e => {
+		console.log(e);
+		console.log(`[postJSON[fetch] 에러 발생]`);
+		console.log(`------------------------------------------------`);
+	})
+}
+
+
+
+
+
+// 회원가입
+document.getElementById('sign_form').addEventListener('submit',
+	function(e) {
+		e.preventDefault();
+		let data = {};
+		const pwcheck = document.querySelector('#sign_pwcheck').value;
+		document.querySelectorAll('#sign_form input').forEach(item => {
+			if (item.name != 'sign_pwcheck') {
+				data[item.name] = item.value;
+			}
+		});
+		for (let item of id_check) {
+			if (item.USER_ID == data.sign_id) {
+				alert("이미 사용중인 ID 입니다.");
+				return;
+			}
+		}
+		for (let key in data) {
+			if (data[key].includes(' ')) {
+				alert("공백을 제외하고 입력해주세요.");
+				return;
+			} else if (sign_pw.value != pwcheck) {
+				alert("PW를 확인해주세요.");
+				return;
+			}
+		}
+		postJSON(data, "sign");
+		// alert("반갑습니다.");
+		document.querySelector(`[name="home_btn"]`).classList.remove('hide');
+		this.closest('.modal').classList.add('hide');
+	});
+
+
+
+
+//
